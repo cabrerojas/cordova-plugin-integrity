749817bd7c0c4ff57742b570e6acdc24e0f5cb5f
Guillermo Cabrera <gcabrerojas@gmail.com>
2025-02-12 11:42:38 -0300

    Delete cordova-plugin-integrity directory

--- cordova-plugin-integrity/www/integrityplugin.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-var IntegrityPlugin = {
-    getAPKHash: function(success, error) {
-        exec(success, error, 'IntegrityPlugin', 'getAPKHash', []);
-    }
-};
-
-module.exports = IntegrityPlugin;
